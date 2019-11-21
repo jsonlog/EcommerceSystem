4e3c1271866fc452d96d0442dd0fadc34083cb58
jsonlog <jsonlogit@gmail.com>
2019-11-21 22:35:48 +0800

    day-17

--- pinyougou-parent/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou-parent/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -67,7 +68,12 @@ public class CartController {
 	}
 	
 	@RequestMapping("/addGoodsToCartList")
+	@CrossOrigin(origins="http://localhost:9105")
 	public Result addGoodsToCartList(Long itemId,Integer num){
+		
+		//response.setHeader("Access-Control-Allow-Origin", "http://localhost:9105");//可以访问的域(当此方法不需要操作cookie)
+		//response.setHeader("Access-Control-Allow-Credentials", "true");//如果操作cookie，必须加上这句话
+		
 		//当前登录人账号
 		String name = SecurityContextHolder.getContext().getAuthentication().getName();
 		System.out.println("当前登录人："+name);
--- pinyougou-parent/pinyougou-pojo/src/main/java/com/pinyougou/pojo/group/Cart.java
+++ pinyougou-parent/pinyougou-pojo/src/main/java/com/pinyougou/pojo/group/Cart.java
@@ -39,6 +39,31 @@ public class Cart implements Serializable{
 	public void setOrderItemList(List<TbOrderItem> orderItemList) {
 		this.orderItemList = orderItemList;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sellerId == null) ? 0 : sellerId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cart other = (Cart) obj;
+		if (sellerId == null) {
+			if (other.sellerId != null)
+				return false;
+		} else if (!sellerId.equals(other.sellerId))
+			return false;
+		return true;
+	}
 	
 	
 	
